9c8d3501a3a829549801c49550ae6414ce1dd77a
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2022-08-29 15:12:47 +0300

    pr3

--- script.js
+++ script.js
@@ -1,73 +1,16 @@
+/** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 800;
-const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 4;
-// let gameFrame = 0;
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
 
-const backgroundLayer1 = new Image();
-backgroundLayer1.src = 'pic/layer-1.png';
-const backgroundLayer2 = new Image();
-backgroundLayer2.src = 'pic/layer-2.png';
-const backgroundLayer3 = new Image();
-backgroundLayer3.src = 'pic/layer-3.png';
-const backgroundLayer4 = new Image();
-backgroundLayer4.src = 'pic/layer-4.png';
-const backgroundLayer5 = new Image();
-backgroundLayer5.src = 'pic/layer-5.png';
+enemy1 = {
+    x: 0,
+    y: 0,
+    width: 200,
+    height: 200,
+}
 
-window.addEventListener('load', function () {
-    const slider = document.getElementById('slider');
-    slider.value = gameSpeed;
-    const showGameSpeed = document.getElementById('showGameSpeed');
-    showGameSpeed.innerHTML = gameSpeed;
-    slider.addEventListener('change', function (e) {
-        gameSpeed = e.target.value;
-        showGameSpeed.innerHTML = e.target.value;
-    });
-
-    class Layer {
-        constructor(image, speedModifier) {
-            this.x = 0;
-            this.y = 0;
-            this.width = 2400;
-            this.height = 700;
-            this.x2 = this.width;
-            this.image = image;
-            this.speedModifier = speedModifier;
-            this.speed = gameSpeed * this.speedModifier;
-        }
-        update() {
-            this.speed = gameSpeed * this.speedModifier;
-            if (this.x <= -this.width){
-                this.x = 0;
-            }
-            this.x = this.x - this.speed;
-            // this.x = gameFrame * this.speed % this.width;
-        }
-        draw() {
-            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-            ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
-        }
-    }
-
-    const layer1 = new Layer(backgroundLayer1, 0.2);
-    const layer2 = new Layer(backgroundLayer2, 0.4);
-    const layer3 = new Layer(backgroundLayer3, 0.6);
-    const layer4 = new Layer(backgroundLayer4, 0.8);
-    const layer5 = new Layer(backgroundLayer5, 1);
-
-    const gameObjects = [layer1, layer2, layer3, layer4, layer5];
-
-    function animate() {
-        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        gameObjects.forEach(object => {
-            object.update();
-            object.draw();
-        });
-        // gameFrame--;
-        requestAnimationFrame(animate);
-    };
-    animate();
-
-});
+function animate(){
+    ctx,fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+}
