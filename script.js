d3d965cc7133b0bb8da4a8e667f51d4cdc4296e4
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2022-09-06 18:42:07 +0300

    prj3/enemy4

--- script.js
+++ script.js
@@ -3,14 +3,59 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 40;
+const enemiesArray = [];
 
-enemy1 = {
-    x: 0,
-    y: 0,
-    width: 200,
-    height: 200,
+let gameFrame = 0;
+
+class Enemy {
+    constructor(){
+        this.image = new Image();
+        this.image.src = 'pic/enemy4.png';
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 213;
+        this.spriteHeight = 213;
+        this.width = this.spriteWidth / 2;
+        this.height = this.spriteHeight / 2;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.newX = Math.random() * canvas.width;
+        this.newY = Math.random() * canvas.height;
+        this.frame = 0; 
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        this.interval = Math.floor(Math.random() * 200 + 50);
+    }
+    update(){
+        if (gameFrame % 60 === 0){
+            this.newX = Math.random() * (canvas.width - this.width);
+        this.newY = Math.random() * (canvas.height - this.height);
+        }
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        this.x -= dx/70;
+        this.y -= dy/70;
+
+        if (this.x + this.width < 0) this.x = canvas.width;
+        // animate sprites
+        if (gameFrame % this.flapSpeed === 0){
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+    draw(){
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight,  this.x, this.y, this.width, this.height);
+    }
+};
+
+for (let i = 0; i < numberOfEnemies; i++){
+    enemiesArray.push(new Enemy());
 }
-
 function animate(){
-    ctx,fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    });
+    gameFrame++;
+    requestAnimationFrame(animate);
 }
+animate();
